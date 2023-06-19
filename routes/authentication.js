ac83c830307d08136ab7a5d11f92b3b6653ee8bf
anouaraissani <aaissani571@gmail.com>
2023-06-20 00:48:48 +0200

    delete the register page code

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const {getLogin, postLogin, getRegister, postRegister, logout} = require('../controllers/authentication')
+const {getLogin, postLogin, logout} = require('../controllers/authentication')
 const {checkNotAuthenticated} = require('../middlewares/authentification')
-const { getConnection, connect, closePool } = require('../db/connect')
+const { getConnection, connect} = require('../db/connect')
 
 //authentication middleware 
 const passport = require('passport')
@@ -69,8 +69,6 @@ router.use(passport.initialize())
 router.use(express.static('./views'))
 //login
 router.route('/login').get(checkNotAuthenticated, getLogin).post(checkNotAuthenticated, postLogin)
-//register
-router.route('/register').get(checkNotAuthenticated, getRegister).post(checkNotAuthenticated, postRegister)
 //logout
 router.get('/logout', logout)
 module.exports = router
