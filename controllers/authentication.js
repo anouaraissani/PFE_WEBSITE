9614cf33143743cf9fcc9ca0e11a442e9c544880
anouaraissani <aaissani571@gmail.com>
2023-06-20 01:50:05 +0200

    delete unused dashboard function

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -22,10 +22,4 @@ const logout = (req, res) => {
     })
 }
 
-const dashboard = async (req, res) => {
-    const user = await req.user
-    console.log('the user name: ',user.name)
-    res.render('index.ejs', { name: user.name })
-}   
-
-module.exports = {getLogin, postLogin, logout, dashboard}
+module.exports = {getLogin, postLogin, logout}
